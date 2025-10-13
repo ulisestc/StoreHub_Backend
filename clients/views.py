9293deef1ec5a352319fa224dd7fab5561f4eda0
ulisestc <ulisestc27@gmail.com>
2025-10-13 01:02:09 -0600

    Se crearon los serializadores y view spara clients

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client
+from .serializers import ClientSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes =  [IsAuthenticated] # Para no poder modificar sin ser usuario
--- storehub_backend/urls.py
+++ storehub_backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('products.urls')),
+    path('api/', include('products.urls')),
+    path('api/', include('clients.urls')),
 ]
