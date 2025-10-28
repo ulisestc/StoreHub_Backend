44c94536253e2866a2cb6dec8e49f07755db9cd6
ulisestc <ulisestc27@gmail.com>
2025-10-27 19:31:35 -0600

    Se agregó busqueda de productos por nombre / sku, además de filtrado por id de categorías

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -14,4 +15,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes =  [IsAuthenticated] # Para no poder modificar sin ser usuario
+
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'sku']  # parametro ?search=X
+    filterset_fields = ['category']  # parametro ?category=X
     
--- storehub_backend/settings.py
+++ storehub_backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'djoser',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -137,6 +138,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    #paginación global !!! se usa con ?page=X
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    #fitlros
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],   
 }
 
 SIMPLE_JWT = {
