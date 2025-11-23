5fb04d521c2d5c9ccbc5ce17adfdbde1c22827cb
ulisestc <ulisestc27@gmail.com>
2025-11-23 15:53:55 -0600

    Se migró a mysql

--- storehub_backend/settings.py
+++ storehub_backend/settings.py
@@ -82,14 +82,16 @@ WSGI_APPLICATION = 'storehub_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, "my.cnf"),
+            'charset': 'utf8mb4',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
