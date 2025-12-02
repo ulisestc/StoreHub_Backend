6471e9b8317321b545035dc9f74b80c003da25f3
ulisestc <ulisestc27@gmail.com>
2025-12-02 13:23:05 -0600

    Se registró User dentro de /admin para que adminstradores puedan acceder al panel de control django y cambiar el rol de otros usuarios

--- sales/serializers.py
+++ sales/serializers.py
@@ -17,17 +17,6 @@ class SaleDetailSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['price_at_sale', 'product_name']
 
-    # def validate(self, data):
-    #     product = data ['product']
-    #     quantity = data ['quantity']
-
-    #     if quantity <= 0:
-    #         raise serializers.ValidationError("La cantidad debe ser mayor a cero.")
-        
-    #     if product.stock < quantity:
-    #         raise serializers.ValidationError("No hay suficiente stock para el producto seleccionado.")
-        
-    #     return data
         
 class SaleSerializer(serializers.ModelSerializer):
     
@@ -54,10 +43,3 @@ class SaleSerializer(serializers.ModelSerializer):
             'details',
         ]
         read_only_fields = ['subtotal','impuestos','total', 'created_at', 'user', 'client_name']
-
-    # def validate(self, data):
-    #     details = data.get('details', [])
-        
-    #     if not details:
-    #         raise serializers.ValidationError("La venta debe tener al menos un detalle.")
-    #     return data
